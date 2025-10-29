94f7a38cb74e00e20e682823036145a08ea2cd09
leonardo <vieiraleo10@hotmail.com>
2025-10-28 22:55:47 -0300

    first commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./login.module.css"
+
+const LoginPage = () => {
+  return (
+    <div className={styles.containerPrincipal}>
+      <h1>Página de Login</h1>
+    </div>
+  )
+}
+
+export default LoginPage;
